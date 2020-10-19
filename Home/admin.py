4f23c29354218190e3e2a12ecd0586e47b4450e9
Gauravi Sharma <gauravisharma1508@gamil.com>
2020-10-19 17:25:23 +0530

    'done'

--- Home/admin.py
+++ Home/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from .models.product import Product
 from .models.category import Category
 
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name','price','category','description']
+
+class CategoryProduct(admin.ModelAdmin):
+    list_display = ['name']
+
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Category)
+admin.site.register(Product,AdminProduct)
+admin.site.register(Category,CategoryProduct)
