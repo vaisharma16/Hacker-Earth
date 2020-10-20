048b15ea7978ac9688b9d00b48305b82e3bcc912
Gauravi Sharma <gauravisharma1508@gamil.com>
2020-10-21 00:44:06 +0530

    'done'

--- Home/views.py
+++ Home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models.product import  Product
 
 
 # Create your views here.
 def index(request):
-    return render(request , 'thewayshop/index.html')
+    pro=Product.get_all_products();
+
+    return render(request , 'thewayshop/index.html' , {'products' : pro})
