bac269e65a04d321e4f6853b0419d2028bc51fff
Gauravi Sharma <gauravisharma1508@gamil.com>
2020-11-05 15:47:03 +0530

    done

--- Store/views.py
+++ Store/views.py
@@ -10,15 +10,15 @@ def index(request):
     #pro=Product.get_all_products();
     categories=Category.get_all_categories()
     categoryID=request.GET.get('category')
-    #subcategories = SubCategory.get_subcategories(categoryID)
-    if categoryID:
-        products=Product.get_all_products_by_categoryid(categoryID)
+    subcategories = SubCategory.get_subcategories(categoryID)
+    #if categoryID:
+        #products=Product.get_all_products_by_categoryid(categoryID)
+        #subcategories = SubCategory.get_subcategories(categoryID)
+    #else:
 
-    else:
-
-        products=Product.get_all_products()
+        #subcategories=Product.get_all_products()
     data={}
-    data['products']=products
+    #data['products']=products
     data['categories']=categories
-    #data['subcategories'] = subcategories
+    data['subcategories'] = subcategories
     return render(request , 'thewayshop/index.html', data)
